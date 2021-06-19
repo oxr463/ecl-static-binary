09851f609d4fd49c7a657b1edffe8aeb691ff925
Lucas Ramage <ramage.lucas@protonmail.com>
2021-06-19 19:37:31 -0400

    Add unix-opts from example
    See: https://github.com/libre-man/unix-opts/blob/master/example/example.lisp#L32

--- src/hello.lisp
+++ src/hello.lisp
@@ -1,3 +1,26 @@
+(opts:define-opts
+  (:name :help
+   :description "print this help text"
+   :short #\h
+   :long "help")
+  (:name :verbose
+   :description "verbose output"
+   :short #\v
+   :long "verbose")
+  (:name :level
+   :description "the program will run on LEVEL level"
+   :required t
+   :short #\l
+   :long "level"
+   :arg-parser #'parse-integer
+   :meta-var "LEVEL")
+  (:name :output
+   :description "redirect output to file FILE"
+   :short #\o
+   :long "output"
+   :arg-parser #'identity
+   :meta-var "FILE"))
+
 (defun hello()
   (format t "Hello, World!~%"))
 
